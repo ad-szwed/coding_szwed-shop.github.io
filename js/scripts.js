32289686ff94884701c2fda061112ca416fe1d09
ad-szwed <shvedatech@gmail.com>
2022-03-15 10:36:29 +0000

    minor content changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,18 @@ let modal = document.querySelector('.modal'),
   modalBody = document.querySelector('.modal p'),
   isModalOpen = false,
   background = document.querySelector('.menu-container'),
+  body = document.body,
   // menu items: 
   menuItem = document.querySelectorAll('.menu-item'),
   hoverAudio = new Audio("./audio/hover.mp3"),
-  modalAudio = new Audio("audio/modalOpen.wav");
+  soundOpen = new Audio("audio/modalOpen.wav"),
+  soundClose = new Audio("audio/modalClose.mp3");
 
 // DATA STRUCTURE
 let modalContent = {
   'about': {
     'title': 'about me',
-    'content': 'ABOUTLorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem suscipit officia excepturi aut labore maxime voluptatibus, eum aspernatur cupiditate nesciunt debitis earum dolore corrupti iste itaque obcaecati optio quidem dolor laboriosam minima veniam voluptate animi perspiciatis. Facilis eveniet mollitia ipsum corrupti officiis explicabo sapiente porro perferendis ad molestiae distinctio ipsa temporibus numquam deserunt quia est sed eius optio, iusto ex dolorum? Dolores rem voluptatum qui aut quaerat ipsam provident eveniet corrupti eos vel minima beatae, molestiae, necessitatibus nobis officiis error modi praesentium repellat quibusdam sed, cupiditate corporis aperiam. Rerum quos maiores quis, iusto fugiat amet odio eius dolor ullam. Asperiores.'
+    'content': "My name is Adam and I have a difficult to prenaunce surname. It suppose to sound like 'shveh-dah', but It's usually prenounced as sweat-ah. I got so used to this misprenaunceation, that I turned it into my pesonal brand - szwed-shop. Coding szwed-shop"
   },
   'work': {
     'title': 'My Work',
@@ -20,11 +22,21 @@ let modalContent = {
   },
   'contact': {
     'title': 'Contact me',
-    'content': 'CONTACTLorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem suscipit officia excepturi aut labore maxime voluptatibus, eum aspernatur cupiditate nesciunt debitis earum dolore corrupti iste itaque obcaecati optio quidem dolor laboriosam minima veniam voluptate animi perspiciatis. Facilis eveniet mollitia ipsum corrupti officiis explicabo sapiente porro perferendis ad molestiae distinctio ipsa temporibus numquam deserunt quia est sed eius optio, iusto ex dolorum? Dolores rem voluptatum qui aut quaerat ipsam provident eveniet corrupti eos vel minima beatae, molestiae, necessitatibus nobis officiis error modi praesentium repellat quibusdam sed, cupiditate corporis aperiam. Rerum quos maiores quis, iusto fugiat amet odio eius dolor ullam. Asperiores.'
+    'content': '<a href="mailto:someone@yoursite.com">E-mail me!</a>'
   },
   'mission': {
     'title': 'My Mission',
     'content': 'MISSIONLorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem suscipit officia excepturi aut labore maxime voluptatibus, eum aspernatur cupiditate nesciunt debitis earum dolore corrupti iste itaque obcaecati optio quidem dolor laboriosam minima veniam voluptate animi perspiciatis. Facilis eveniet mollitia ipsum corrupti officiis explicabo sapiente porro perferendis ad molestiae distinctio ipsa temporibus numquam deserunt quia est sed eius optio, iusto ex dolorum? Dolores rem voluptatum qui aut quaerat ipsam provident eveniet corrupti eos vel minima beatae, molestiae, necessitatibus nobis officiis error modi praesentium repellat quibusdam sed, cupiditate corporis aperiam. Rerum quos maiores quis, iusto fugiat amet odio eius dolor ullam. Asperiores.'
+  }
+}
+
+// sounds rules
+function tone(sound, toggle) {
+  if (toggle) {
+    // cloneNode(true) before .play() makes it possible to play it multiple times at once!
+    sound.cloneNode(true).play()
+  } else {
+    sound.play()
   }
 }
 
@@ -34,6 +46,7 @@ function modalToggle() {
     modal.classList.add('show');
     // console.log(background.classList)
     background.classList.add('dim');
+    body.style.background = 'url("/img/pic.jpg")';
     isModalOpen = !isModalOpen;
   } else {
     modal.classList.remove('show');
@@ -58,7 +71,7 @@ menuItem.forEach((elem) => {
     console.log(target, '!!target');
     modalTitle.innerHTML = modalContent[target].title;
     modalBody.innerHTML = modalContent[target].content;
-    modalAudio.cloneNode(true).play();
+    tone(soundOpen, true)
   });
 })
 
@@ -66,6 +79,7 @@ menuItem.forEach((elem) => {
 document.addEventListener('click', () => {
   if (isModalOpen) {
     modalToggle();
+    tone(soundClose, true);
   }
 })
 
@@ -76,9 +90,6 @@ document.addEventListener('keydown', (event => {
     isModalOpen = !isModalOpen;
   }
 }))
-
-// play sound on hover
-
 
 // ===================================*  ACTUAL CONTENT FOR MODALS  *========================================
 
